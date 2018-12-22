06388391be89b5e0634c694a651b8604ea810bdc
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-22 02:37:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,22 +7,25 @@ function nestedTarget(cla, id) {
 }
 
 function deepestChild() {
-  let node = document.getElementById( '.grand-node' )
-  let nextNode = node.children[0]
-  while (nextNode)  {
+  let node = document.getElementById( 'grand-node' )
+  let nextNode = node.children[ 0 ]
+
+  while ( nextNode ) {
     node = nextNode
-    nextNode = node.children[0]
+    nextNode = node.children[ 0 ]
   }
+
   return node
 }
 
-function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
+function increaseRankBy( n ) {
+  const rankedLists = document.querySelectorAll( '.ranked-list' )
 
-  for ( i = 0 , l = rankedLists.length ; i < l ; i++ ) {
-    let children = rankedLists[i].children
-    for ( let j = 0 , k = children.length ; j < k ; j++ ) {
-      children[j].innerHTML = parseInt( children[j].innerHTML ) + n
+  for ( let i = 0, l = rankedLists.length; i < l; i++ ) {
+    let children = rankedLists[ i ].children
+
+    for ( let j = 0, k = children.length; j < k; j++ ) {
+      children[ j ].innerHTML = parseInt( children[ j ].innerHTML ) + n
     }
   }
 }
