c8b35577510fdc39e6cbe683efc4b5b6f7c3a97d
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-22 02:37:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ function nestedTarget(cla, id) {
   return document.querySelector('#nested .target');
 }
 
+function deepestChild() {
+  let node = document.getElementById( '.grand-node' )
+  let nextNode = node.children[0]
+  while (nextNode)  {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  return node
+}
+
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
 
@@ -15,14 +25,4 @@ function increaseRankBy(n) {
       children[j].innerHTML = parseInt( children[j].innerHTML ) + n
     }
   }
-}
-
-function deepestChild() {
-  let node = document.getElementById( '.grand-node' )
-  let nextNode = node.children[0]
-  while (nextNode)  {
-    node = nextNode
-    nextNode = node.children[0]
-  }
-  return node
 }
