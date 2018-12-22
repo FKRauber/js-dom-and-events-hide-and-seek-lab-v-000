d91e76b1aeb2640c7cfc01aa635b7c83dc79c1d0
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-22 02:33:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function increaseRankBy(n) {
 
   for ( i = 0 , l = rankedLists.length ; i < l ; i++ ) {
     let children = rankedLists[i].children
-    for ()
+    for ( let j = 0 , k = children.length ; j < k ; j++ ) {
+      children[j].innerHTML = parseInt( children[j].innerHTML ) + n
+    };
   };
 }
